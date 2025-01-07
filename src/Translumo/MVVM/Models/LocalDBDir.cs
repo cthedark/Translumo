ad3375d9d31e082bb8e5ebe7ace176e9f92f27b3
Charles Ko <1108985+cthedark@users.noreply.github.com>
2025-01-07 08:14:40 -0800

    damn windows bruh

---
+++ src/Translumo/MVVM/Models/LocalDBDir.cs
@@ -0,0 +1,10 @@
+namespace Translumo.MVVM.Models
+{
+    public class LocalDBDir
+    {
+        public string Value { get; set;}
+        override public string ToString() {
+            return Value;
+        }
+    }
+}
